8beca07070ff13204bf621f850784f2020d4f0a5
Stefi Rosadi Kusuma Putri <stefi.putri@tiket.com>
2022-09-13 22:48:43 +0700

    chore: add app.json & intl packages

--- src/lib/utils/currency-format.ts
+++ src/lib/utils/currency-format.ts
@@ -1,3 +1,6 @@
+import "intl";
+import "intl/locale-data/jsonp/en";
+
 export const formatCurrency = (currency: string, value: number) => {
   return `${currency} ${new Intl.NumberFormat('id-ID', {
     maximumFractionDigits: 0,
